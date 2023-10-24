31226e6946bd3ddb3115cae4ec14a5f552a13251
Jackson Magas <jacksonmagas@gmail.com>
2023-10-24 12:02:43 -0400

    Rename tokenizer.h to tokenize_all.h

---
+++ tokenize_all.h
@@ -0,0 +1,17 @@
+//Header File for tokenizer
+#ifndef _TOKENIZER_H
+#define _TOKENIZER_H
+
+//reads a string until the next quote
+//returns the length of the output string
+int read_quote_string(char* input, char* output);
+
+//read a string until the next special character or space
+//returns the length of the output string
+int read_nonspecial_string(char* input, char* output);
+
+//determines if the character is one of:
+//(, ), <, >, ;, |
+int is_special_character(char input);
+
+#endif /*#ifndef _TOKENIZER_H */
