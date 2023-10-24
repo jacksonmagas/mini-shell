f59d77d3bbe5da31e036152560a85bf9569a9eb4
Jackson Magas <jacksonmagas@gmail.com>
2023-10-24 14:52:57 -0400

    Comment tokenize.c further

--- tokenize.c
+++ tokenize.c
@@ -8,26 +8,33 @@
  * Allocates memory for 256 strings of length 256, it is the responsability of callers of tokenize to free the memory.
  */
 char** tokenize(char* inputString) {
+  //allocate memory for array of up to 256 tokens of up to 256 bytes each
   char** output = calloc(256, sizeof(char*));
   for (int i = 0; i < 256; i++) {
     output[i] = calloc(256, sizeof(char));
   }
   int charNum = 0;
   int tokenNum = 0;
+  //iterate through the string adding each token to the output
   while (inputString[charNum] != '\0') {
     char buf[256];
     if (inputString[charNum] == '"') {
+      //precrement charNum to pass first quote
       ++charNum;
       int len = read_quote_string(&inputString[charNum], buf);
+      //add length of the quote string to index to avoid double reading
       charNum += len;
     } else if (is_special_character(inputString[charNum])) {
+      //for special characters simply add the character and a null character to the buffer
       buf[0] = inputString[charNum];
       buf[1] = '\0';
       charNum++;
     } else if (inputString[charNum] != ' ') {
+      //if it's none of the above and not a space read the string into the buffer
       int len = read_nonspecial_string(&inputString[charNum], buf);
       charNum += len;
     } else {
+      //if it's a space just move on to the next letter.
       charNum++;
       continue;
     }
@@ -43,19 +50,26 @@ char** tokenize(char* inputString) {
   return output;
 }
 
+
 int main(int argc, char **argv) {
+  //get up to 256 characters of input from stdin
   char* inputString = calloc(256, sizeof(char));
   fgets(inputString, 256, stdin);
+  //remove trailing newline from gets
   inputString[strcspn(inputString, "\r\n")] = 0;
+  
+  //tokenize string
   char** tokens = tokenize(inputString);
+  //print each token on a new line
   int i = 0;
   while (tokens[i] != NULL) {
     if (i > 0) { puts("");}
     printf("%s", tokens[i]);
     ++i;
   }
-  for (int i = 0; i < 256; i++) {
-    free(tokens[i]);
+  //free all memory
+  for (int j = 0; j < 256; j++) {
+    free(tokens[j]);
   }
   free(tokens);
   free(inputString);
