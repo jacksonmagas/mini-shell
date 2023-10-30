82aa62b2d0b095d870d7bde170eaa9e998bb27ca
Jackson Magas <jacksonmagas@gmail.com>
2023-10-30 18:01:36 -0400

    Update header files to work

--- shell.c
+++ shell.c
@@ -5,57 +5,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-char** tokenize(char* inputString) {
-  //allocate memory for array of up to 256 tokens of up to 256 bytes each
-  char** output = calloc(256, sizeof(char*));
-  int charNum = 0;
-  int tokenNum = 0;
-  //iterate through the string adding each token to the output
-  while (inputString[charNum] != '\0') {
-    char buf[256];
-    if (inputString[charNum] == '"') {
-      //precrement charNum to pass first quote
-      ++charNum;
-      int len = read_quote_string(&inputString[charNum], buf);
-      //add length of the quote string to index to avoid double reading
-      charNum += len;
-    } else if (is_special_character(inputString[charNum])) {
-      //for special characters simply add the character and a null character to the buffer
-      buf[0] = inputString[charNum];
-      buf[1] = '\0';
-      charNum++;
-    } else if (inputString[charNum] != ' ') {
-      //if it's none of the above and not a space read the string into the buffer
-      int len = read_nonspecial_string(&inputString[charNum], buf);
-      charNum += len;
-    } else {
-      //if it's a space just move on to the next letter.
-      charNum++;
-      continue;
-    }
-    int j = 0;
-    output[tokenNum] = calloc(strlen(buf) + 1, sizeof(char));
-    while (buf[j] != '\0') {
-      output[tokenNum][j] = buf[j];
-      ++j;
-    }
-    ++tokenNum;
-  }
-  output[tokenNum + 1] = NULL;
-  return output;
-}
-
-char** getInputTokens() {
-  //get up to 256 characters of input from stdin
-  char* inputString = calloc(256, sizeof(char));
-  fgets(inputString, 255, stdin);
-  //remove trailing newline from gets
-  inputString[strcspn(inputString, "\r\n")] = 0;
-  //tokenize string
-  char** tokens = tokenize(inputString);
-  return tokens;
-}
-
 int main(int argc, char **argv) {
   
   // Prints welcome message
