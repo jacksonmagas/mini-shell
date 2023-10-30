3ca0e9f735033ab0b29986bd50a1ba7d64a6f210
Jackson Magas <jacksonmagas@gmail.com>
2023-10-30 18:37:51 -0400

    Fix shell to search path instead of always prepending bin.

--- shell.c
+++ shell.c
@@ -13,19 +13,16 @@ int main(int argc, char **argv) {
   // Obtains intial input
   char** input = getInputTokens();
 
-  // Loops shell until exit
+  // Losrcops shell until exit
   while (input[0] != NULL && strcmp(input[0], "exit") != 0) {
     char* myargv[16];
     char* src = calloc(256, sizeof(char));
-    char* dest = calloc(256, sizeof(char));
 
     // Processes the tokens as arguments
     for (int i = 0; i <= sizeof(input); i++) {
       if (i == 0) {
         strcpy(src, input[0]);
-        strcpy(dest, "/bin/");
-        strcat(dest, src);
-        myargv[0] = dest;
+        myargv[0] = src;
       } else if (i == sizeof(input)) {
         myargv[i] = NULL;
       } else {
@@ -35,7 +32,7 @@ int main(int argc, char **argv) {
 
     // Runs the arguments
     if (fork() == 0) {
-      execve(myargv[0], myargv, NULL);
+      execvp(myargv[0], myargv);
       strcat(src, ": command not found\n");
       printf("%s", src);
       exit(1);
@@ -47,6 +44,7 @@ int main(int argc, char **argv) {
     // Prepares the shell for next input arguments
     printf("shell $ ");
     input = getInputTokens();
+    free(src);
   }
 
   // Prints exit message
@@ -54,6 +52,7 @@ int main(int argc, char **argv) {
     printf("\n");
   }
   printf("Bye bye.\n");
+  
 
   return 0;
 }
