c67bf8ae09c5d35737946421d21b3c1d2d7e5ca6
Student User <user@benaronow227-cs3650fa23-vm.khoury.northeastern.edu>
2023-11-01 19:21:24 +0000

    reversed changes

--- shell.c
+++ shell.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <tokenize_all.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,12 +17,10 @@ int getNumElements(char** arr) {
   return numElements;
 }
 
-//fork a child and execute argv[0] with argv as arguments, src as input file, and filename as output file
 void runArgs(char* myargv[16], char* src, char* filename) {
   if (fork() == 0) {
     if (strcmp(filename, "") != 0) {
-      //open the given file as write only, if the file needs to be created create it as rw-r--r--
-      int file_desc = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+      int file_desc = open(filename, O_WRONLY | O_APPEND | O_TRUNC | O_CREAT, 0644);
       dup2(file_desc, 1);
     }
     execvp(myargv[0], myargv);
@@ -34,7 +33,6 @@ void runArgs(char* myargv[16], char* src, char* filename) {
   }
 }
 
-//makes newInput contain the all the elements of input after element i + jump - 1
 void nextArgs(char** input, char** newInput, int i, int jump) {
   int n = 0;
   for (int j = i + jump; j < getNumElements(input); j++) {
@@ -43,40 +41,71 @@ void nextArgs(char** input, char** newInput, int i, int jump) {
   }
 }
 
-//runs the given input string in the shell
+void getFileInput(char** input, int i, char** myargv) {
+  FILE* file = fopen(input[i + 1], "r");
+  char* buffer = NULL;
+  size_t len;
+  ssize_t bytes_read = getdelim(&buffer, &len, '\0', file);
+  if (bytes_read != -1) {
+    myargv[i] = buffer;
+  }
+  //char fileInputString[256] = "";
+  //char s[256];
+  //while (fgets(s, 256, file) != NULL) {
+    //strcat(fileInputString, s);
+  //}
+  //myargv[i] = fileInputString;
+  //char** fileInput = getInputTokens(fileInputString);
+  //for (int j = 0; j <= getNumElements(fileInput); j++) {
+    //if (j == getNumElements(fileInput)) {
+      //myargv[j + i] = NULL;
+    //}
+    //myargv[j + i] = fileInput[j];
+  //}
+}
+
 void parseInput(char** input) {
   char** myargv = calloc(256, sizeof(char*));
   char* src = calloc(256, sizeof(char));
   char** newInput = calloc(256, sizeof(char*));
-  char* filename = "";
-  //copy first element into input list
-  strcpy(src, input[0]);
-  myargv[0] = src;
   // Processes the tokens as arguments
   for (int i = 0; i <= getNumElements(input); i++) {
-    if (i == getNumElements(input)) {
-      //when you reach the end of the input null terminate and try to run
+    if (i == 0) {
+      strcpy(src, input[0]);
+      myargv[0] = src;
+    } else if (i == getNumElements(input)) {
       myargv[i] = NULL;
-      runArgs(myargv, src, filename);
+      runArgs(myargv, src, "");
     } else if (strcmp(input[i], ";") == 0) {
-      //handle sequencing: null terminate and run the first command
       myargv[i] = NULL;
-      runArgs(myargv, src, filename);
-      //handle sequencing: get the rest of the input after the ; and recursively call parseinput on that
+      runArgs(myargv, src, "");
       if (i < getNumElements(input) - 1) {
         nextArgs(input, newInput, i, 1);
         parseInput(newInput);	
       }
       break;
     } else if (strcmp(input[i], ">") == 0) {
-      //handle output redirection by updating filename
-      filename = input[i + 1];
-      i++;
+      myargv[i] = NULL;
+      if (i < getNumElements(input) - 1) {
+        runArgs(myargv, src, input[i + 1]);
+      }
+      if (i < getNumElements(input) - 3 && strcmp(input[i + 2], ";") == 0) {
+        nextArgs(input, newInput, i, 3);
+	parseInput(newInput);
+      }
+      break;
     } else if (strcmp(input[i], "<") == 0) {
-      //handle input redirection by updating src
-      
+      if (i < getNumElements(input) - 1) {
+        getFileInput(input, i, myargv);
+        myargv[i + 1] = NULL;	
+	runArgs(myargv, src, "");
+      }
+      if (i < getNumElements(input) - 3 && strcmp(input[i + 2], ";") == 0) {
+        nextArgs(input, newInput, i, 3);
+        parseInput(newInput);
+      }
+      break;
     } else if (strcmp(input[i], "|") == 0) {
-      //handle piping
       break;
     } else {
       myargv[i] = input[i];
@@ -93,10 +122,11 @@ int main(int argc, char **argv) {
   printf("Welcome to mini-shell.\nshell $ ");
   
   // Obtains intial input
-  char** input = getInputTokens();
+  char** input = getInputTokens("");
 
   // Loops shell until exit
   while (input[0] != NULL && strcmp(input[0], "exit") != 0) {
+    char* filenameList = "";
     
     // Reacts to user input if not only a newline
     if (strcmp(input[0], "\n") != 0) {
@@ -105,7 +135,7 @@ int main(int argc, char **argv) {
 
     // Prepares the shell for next input arguments
     printf("shell $ ");
-    input = getInputTokens();
+    input = getInputTokens("");
   }
 
   // Prints exit message
